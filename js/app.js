d8ddc0eff324be7321aea72f6f3a6eb003e6420c
AbuKausar007 <a.25kausar@gmail.com>
2022-03-02 14:39:16 +0600

    color combination done

--- js/app.js
+++ js/app.js
@@ -46,8 +46,8 @@ const mobileSearchResult = mobiles => {
         <div class="card border-0 rounded-5 shadow-lg w-75 mx-auto text-center py-3">
             <img class="w-75 mx-auto" src="${mobile.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h3 class="card-title fw-bold text-color">${mobile.phone_name}</h3>
-            <p class="brand-name text-color fw-bold fs-5">Brand: <span class="fw-bold orange-color">${mobile.brand}</span></p>
+            <h3 class="card-title fw-bold blue-color">${mobile.phone_name}</h3>
+            <p class="brand-name blue-color fw-bold fs-5">Brand: <span class="fw-bold orange-color">${mobile.brand}</span></p>
             <button onclick="loadMobileDetail('${mobile.slug}')" id="details-button" class="btn py-2 px-4 fw-bold text-white rounded-5">See More</button>
         </div>
         </div>
